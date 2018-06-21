1abfe63b55fc254822814b39b28e758671510ecf
BuildTools <unconfigured@null.spigotmc.org>
2018-06-21 23:38:00 +0300

    Added Cache

--- src/main/java/me/brokenearth/core/cache/Cache.java
+++ src/main/java/me/brokenearth/core/cache/Cache.java
@@ -14,6 +14,8 @@ import java.util.Map;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  *
+ * @author BrokenEarth // BrokenEarthDev
+ * @version 1.0
  * @param <K> The key
  * @param <V> The value
  */
